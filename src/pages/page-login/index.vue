c04640bd85b741c802109660fbccd211024ca6a6
xiaoming <1637077309@qq.com>
2017-11-10 17:58:06 +0800

    登录验证和拦截跳转

--- src/pages/page-login/index.vue
+++ src/pages/page-login/index.vue
@@ -26,7 +26,52 @@
       </table>
     </div> -->
     <div class="top-bar">
+      <div class="login-container">
+        <i class="iconfont icon-logo1"></i>
+      </div>
+    </div>
+    <div class="login-area">
+      <div class="left-circle"></div>
+      <div class="right-circle"></div>
 
+      <div class="login-container relative">
+        <div class="login-box">
+          <div class="shadow"></div>
+          <h1>登录</h1>
+          <div class="login-form">
+            <form>
+              <div class="login-row">
+                <label for="">用户名</label>
+                <div class="login-input">
+                  <input size="large" placeholder="用户名" v-model="loginForm.loginName"></input>
+                </div>
+              </div>
+              <div class="login-row">
+                <label for="">密码</label>
+                <div class="login-input">
+                  <input size="large" placeholder="密码" v-model="loginForm.loginPwd"></input>
+                </div>
+              </div>
+              <div class="login-row">
+                <label for="">验证码</label>
+                <div class="login-input">
+                  <input size="large" placeholder="验证码" v-model="loginForm.loginPwd" style="width: 232px;"></input>
+                  <img class="verify-code" src="./img/code.png" alt="">
+                </div>
+              </div>
+              <div class="login-row" style="padding-top: 10px;">
+                <span class="text-danger" v-if="errorInfo"><Icon type="close-circled"></Icon> {{errorInfo}}</span>
+              </div>
+              <div class="login-row">
+                <Button style="padding:10px 8px" type="primary" size="large" long>登录</Button>
+              </div>
+              <div class="login-row text-center" style="padding-top: 10px;">
+                <a href="#">忘记密码？</a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -38,6 +83,7 @@ export default {
       loading: false,
       fromOtherPage: false,
       redirectUrl: '',
+      errorInfo: '用户名或密码错误',
       loginForm: {
         loginName: 'test',
         loginPwd: '111111'
@@ -71,23 +117,5 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-  .login-box{
-    width: 400px;
-    height: 300px;
-    background-color: #f4f4f4;
-    margin: 200px auto;
-    padding:40px 20px;
-    border-radius: 5px;
-    td{
-      padding: 10px 5px;
-    }
-  }
-  .top-bar{
-    height: 40px;
-    background-color: #48a6f8;
-    .main-container{
-      width: 1200px;
-      margin: 0 auto;
-    }
-  }
+  @import './style.scss';
 </style>
