92c2df7e1d7fbb2c169957e34c64abaa43d72a25
xiaoming <1637077309@qq.com>
2017-12-29 13:25:05 +0800

    业务审批

---
+++ src/pages/loan-business/business-approve/status-enum.js
@@ -0,0 +1,45 @@
+export default {
+  'groupKey': 'LoanBizNodeEnum',
+  'items': [
+    {
+      'itemCode': '1',
+      'itemName': '录单'
+    },
+    {
+      'itemCode': '2',
+      'itemName': '征信查询'
+    },
+    {
+      'itemCode': '3',
+      'itemName': '初审'
+    },
+    {
+      'itemCode': '4',
+      'itemName': '一级审批'
+    },
+    {
+      'itemCode': '5',
+      'itemName': '二级审批'
+    },
+    {
+      'itemCode': '6',
+      'itemName': '合同制作'
+    },
+    {
+      'itemCode': '7',
+      'itemName': '合同复核'
+    },
+    {
+      'itemCode': '8',
+      'itemName': '合同签署确认'
+    },
+    {
+      'itemCode': '9',
+      'itemName': '放款申请'
+    },
+    {
+      'itemCode': '99',
+      'itemName': '归档完成'
+    }
+  ]
+};
