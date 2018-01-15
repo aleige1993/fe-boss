f0b4fe11974d537488257376d1dcc4404063c63f
15123334187 <445203253@qq.com>
2018-01-15 18:22:35 +0800

    车型管理参数配置联调，上传图片模块优化

--- src/router/index.js
+++ src/router/index.js
@@ -94,7 +94,7 @@ let MyRouter = new Router({
                 // 配置管理-资方管理-合同模板维护
                 { path:'contract', name: 'contract', component: resolve => { require(['@/pages/configuration-manage/manage-invest/contract'], resolve) } },
                 // 配置管理-资方管理-合同模板属性配置
-                { path:'contractsetting', name: 'contractsetting', component: resolve => { require(['@/pages/configuration-manage/manage-invest/contract-setting'], resolve) } },
+                { path:'contractsetting', name: 'contractSetting', component: resolve => { require(['@/pages/configuration-manage/manage-invest/contract-setting'], resolve) } },
               ]
             },// 配置管理-资方管理end
             // 车型管理
