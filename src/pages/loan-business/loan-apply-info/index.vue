eb65bfebd210e727921610eaf8944b68184a9304
01480970 <lihuiyang@haier.com>
2018-02-02 19:04:57 +0800

    用款形式 放开必填校验 仅测试

--- src/pages/loan-business/loan-apply-info/index.vue
+++ src/pages/loan-business/loan-apply-info/index.vue
@@ -167,7 +167,7 @@
               label="用款形式"
               :rules="{required: true, message: '请选择用款形式', trigger: 'change'}"
               prop="loandType">
-              <i-select v-model="formData.loandType" :disabled="true">
+              <i-select v-model="formData.loandType">
                 <i-option value="1">贷款买车</i-option>
                 <i-option value="2">有车贷款</i-option>
               </i-select>
