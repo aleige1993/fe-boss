73ccf5b35fb6a3524eeeedc5d570efb98c2574d7
01480970 <lihuiyang@haier.com>
2018-02-24 09:38:19 +0800

    toString()引起的bug

--- src/pages/configuration-manage/limit-estimate/setting/mixin-data.js
+++ src/pages/configuration-manage/limit-estimate/setting/mixin-data.js
@@ -39,7 +39,7 @@ export default {
                 style: { marginRight: '5px' },
                 on: {
                   click: () => {
-                    params.row.score = params.row.score.toString();
+                    params.row.score = params.row.score + '';
                     this.$data.isAdd = false;
                     this.$data.addModal = true;
                     this.$data.fromData = $.extend({}, params.row);
