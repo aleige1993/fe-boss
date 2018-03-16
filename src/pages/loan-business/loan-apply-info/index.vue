c984f81dc4d380bef41bb12a88f9de1f8f96d457
01480970 <lihuiyang@haier.com>
2018-03-16 11:16:08 +0800

    用款形式 又改成了1.贷款买车；2.有车贷款

--- src/pages/loan-business/loan-apply-info/index.vue
+++ src/pages/loan-business/loan-apply-info/index.vue
@@ -180,8 +180,8 @@
               :rules="{required: true, message: '请选择用款形式', trigger: 'change'}"
               prop="loandType">
               <i-select :disabled="readonly" v-model="formData.loandType">
-                <i-option value="1">有车贷款</i-option>
-                <i-option value="2">贷款买车</i-option>
+                <i-option value="1">贷款买车</i-option>
+                <i-option value="2">有车贷款</i-option>
               </i-select>
             </i-form-item>
           </i-col>
