23d10a796e936c7757e2940f5c0c5445021ef684
01480970 <lihuiyang@haier.com>
2018-03-27 19:02:06 +0800

    bug修复

--- src/pages/configuration-manage/manage-invest/maintain/maintain-add-set-modal/mixin-methods.js
+++ src/pages/configuration-manage/manage-invest/maintain/maintain-add-set-modal/mixin-methods.js
@@ -3,10 +3,10 @@ export default {
     selectCensusDistance(distance) {
       this.$data.formMaintain.bizProvinceCode = distance.provinceCode;
       this.$data.formMaintain.bizProvinceName = distance.provinceName;
-      this.$data.formMaintain.bizCityCode = distance.districtCode;
-      this.$data.formMaintain.bizCityName = distance.districtName;
-      this.$data.formMaintain.bizDistrictCode = distance.cityCode;
-      this.$data.formMaintain.bizDistrictName = distance.cityName;
+      this.$data.formMaintain.bizDistrictCode = distance.districtCode;
+      this.$data.formMaintain.bizDistrictName = distance.districtName;
+      this.$data.formMaintain.bizCityCode = distance.cityCode;
+      this.$data.formMaintain.bizCityName = distance.cityName;
     }
   }
 };
