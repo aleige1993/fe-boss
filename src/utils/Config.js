4dc285c5383d877d98d19180d535ede0dde0ad88
肖洒 <445203253@qq.com>
2018-05-07 09:48:42 +0800

    Config.js

---
+++ src/utils/Config.js
@@ -0,0 +1,7 @@
+export default {
+  HTTPMOCK_ON: process.env.HTTPMOCK_ON,
+  // ENV: process.env.ENV,
+  HTTPMOCK_TIMEOUT: process.env.HTTPMOCK_TIMEOUT,
+  XHRLOG: process.env.XHRLOG, // 是否console出ajax的详细信息；右上角的Notice是否打印url和code
+  HTTPBASEURL: process.env.HTTPBASEURL // 开发服务器
+};
