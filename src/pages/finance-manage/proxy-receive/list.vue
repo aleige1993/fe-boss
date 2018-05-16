af5c72be137d6e2847a38ec767d36008c3a88463
肖洒 <445203253@qq.com>
2018-05-16 17:23:17 +0800

    隐藏贷款申报用途字段

--- src/pages/finance-manage/proxy-receive/list.vue
+++ src/pages/finance-manage/proxy-receive/list.vue
@@ -36,7 +36,7 @@
           <bs-datepicker v-model="searchForm.receiveETime" type="text" placeholder="查询时间"></bs-datepicker>
         </i-form-item>
         <i-form-item prop="password">
-          <i-select :disabled="isDetail" style="width: 120px;" v-model="searchForm.orderStat" placeholder="扣款状态">
+          <i-select style="width: 120px;" v-model="searchForm.orderStat" placeholder="扣款状态">
             <i-option value="" style="height: 26px; color: #bbbec4">-请选择-</i-option>
             <i-option v-for="item in certTypeEnum" :value="item.itemCode" :key="item.itemCode">{{item.itemName}}</i-option>
           </i-select>
