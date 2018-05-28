89f75fbf2b76f6333a27bf1d3889957a0e1c3b04
songchewang <huangl@songchedai.com>
2018-05-28 14:40:01 +0800

    个人客户列表-已婚-填写配偶信息=联调完成

--- config/dev.env.js
+++ config/dev.env.js
@@ -8,7 +8,7 @@ module.exports = merge(prodEnv, {
   HTTPMOCK_ON: false,
   HTTPMOCK_TIMEOUT: 500,
   XHRLOG: true, // 是否console出ajax的详细信息；右上角的Notice是否打印url和code
-   // HTTPBASEURL: '"http://10.164.238.14:9093"' // 开发服务器
-  HTTPBASEURL: '"http://10.164.238.15:8080"' // 测试服务器
+  HTTPBASEURL: '"http://10.164.238.14:9093"' // 开发服务器
+  //HTTPBASEURL: '"http://10.164.238.15:8080"' // 测试服务器
   // HTTPBASEURL: '"http://192.168.201.200:8089"' // 厉洪叶
 });
