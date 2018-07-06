f8bb24e612c0647b48ef448cb46f8ca3c5f10279
肖洒 <445203253@qq.com>
2018-07-06 18:49:07 +0800

    查看大图优化

--- src/components/bs-big-img-2/index.vue
+++ src/components/bs-big-img-2/index.vue
@@ -1,22 +1,18 @@
 <template>
-  <div class="bs-big-img2">
-    <div class="image-dialog">
-      <img class="image-viewer" @click="imageView(thumb + imageHowCut)"
-           :width="thumbWidth" :height="thumbHeight" :original="thumb" :src="thumb + imageHowCut" alt="">
-      <slot name="icon-remove"></slot>
-    </div>
-    <ul class="image-viewer-container" :id="viewId">
-      <li v-for="image in imageList">
-        <img :width="thumbWidth" :height="thumbHeight" :data-original="image.original" :src="image.src" alt="">
+  <div class="image-dialog" style="display: inline-block; position: relative">
+    <ul :id="vmId">
+      <li style="cursor: pointer">
+        <img :width="thumbWidth" :height="thumbHeight" :data-original="full" :src="thumb" alt="">
       </li>
     </ul>
+    <slot name="icon-remove"></slot>
   </div>
 </template>
 <script>
-  import Viewer from 'viewerjs';
+  // import {} from '../../assets/js/viewer-jquery.min';
   import Tools from '@/utils/Tools';
   export default {
-    name: 'bs-big-img2',
+    name: '',
     props: {
       thumb: String,
       full: String,
@@ -26,43 +22,26 @@
     },
     data() {
       return {
-        viewId: 'image-viewer-container-' + Tools.generateUUID(),
-        imageHowCut: '?x-oss-process=image/resize,h_128',
-        imageList: []
+        vmId: ''
       };
     },
-    methods: {
-      imageView(thumb) {
-        let _this = this;
-        _this.$data.imageList = [];
-        $('.image-viewer').each(function() {
-          let $this = $(this);
-          _this.$data.imageList.push({
-            original: $this.attr('original'),
-            src: $this.attr('src')
-          });
-        });
-        _this.$nextTick(function() {
-          _this.Viewer = new Viewer(document.getElementById(_this.$data.viewId), {
-            url: 'data-original',
-            loading: true
-          });
-          $('img[src="' + thumb + '"]', '#' + _this.$data.viewId).trigger('click');
-        });
+    methods: {},
+    mounted() {
+      if (!$.fn.viewer) {
+        require('../../assets/js/viewer-jquery.min');
       }
+      let _id = `bs-big-img-${Tools.generateUUID()}`;
+      this.$data.vmId = _id;
+      setTimeout(function() {
+        $('#' + _id).viewer();
+        // viewer.show();
+      }, 500);
     }
   };
 </script>
 <style lang="scss">
-  @import '../../assets/style/viewer.min.css';
-  .image-dialog {
-    position: relative;
-    display: inline-block;
-  }
-  .image-viewer {
-    cursor: pointer;
-  }
-  .image-viewer-container {
-    display: none;
-  }
+@import '../../assets/style/viewer.min.css';
+.viewer-prev, .viewer-next{
+  display: none !important;
+}
 </style>
