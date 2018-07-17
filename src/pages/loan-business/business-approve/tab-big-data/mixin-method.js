e190ca33ed3b51b5d560d810ce78b2b2d89233ed
songchewang <huangl@songchedai.com>
2018-07-17 16:48:13 +0800

    删除联系人

--- src/pages/loan-business/business-approve/tab-big-data/mixin-method.js
+++ src/pages/loan-business/business-approve/tab-big-data/mixin-method.js
@@ -1,5 +1,34 @@
 export default {
   methods: {
+    // 删除联系人
+    deleteContents() {
+      Alertify.confirm('请仅在客户使用业务员的手机号进件的情况下，需废弃该订单重新进件时，进行此操作！删除"联系人信息"后，该贷款客户的所有联系人信息将被删除并不能恢复。请谨慎删除', async(ok) => {
+        if (ok) {
+          const loading = this.$Message.loading('处理中...', 0);
+          let resp = await this.$http.post('/common/cleanContacts', {
+            memberNo: this.applyBasicInfo.memberNo
+          });
+          loading();
+          if (resp.success) {
+            this.$data.emergencyDatas = [];
+            this.$data.contactBookDatas = [];
+            this.$data.callInDatas = [];
+            this.$data.callOutDatas = [];
+            this.$Message.success('删除成功');
+          }
+        }
+      });
+    },
+    /*
+     * 头部数组
+     */
+    ValidaQuery() {
+      let from = this.$route.query.from;
+      let status = this.$route.query.status;
+      if (from === undefined && status === '3') {
+        this.$data.isShow = true;
+      }
+    },
     async getEmergencyContact(page = 1) {
       this.$data.loadingContactBook = true;
       let resp = await this.$http.post('/common/listUrgencyPhone', {
